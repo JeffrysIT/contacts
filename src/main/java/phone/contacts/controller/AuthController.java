34889a769d2da0819605be66d0629bd02472a4d8
Yurii Ivanyshchev <Megatomua@gmail.com>
2021-07-11 18:17:14 +0300

    updated controllers

--- src/main/java/phone/contacts/controller/AuthController.java
+++ src/main/java/phone/contacts/controller/AuthController.java
@@ -1,17 +1,17 @@
 package phone.contacts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 import phone.contacts.model.User;
 import phone.contacts.security.JwtProvider;
 import phone.contacts.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author Yuriy Ivanischev
@@ -22,30 +22,30 @@ import java.util.Map;
 public class AuthController {
 
     @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
     private UserService userService;
 
     @Autowired
     private JwtProvider jwtProvider;
 
-    @PostMapping(value = "/auth/registration")
-    public void register(WebRequest request) {
+
+    @PostMapping(value = "/auth/register")
+    public ResponseEntity<?> register(HttpServletRequest request) {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        User newUser = new User();
-        newUser.setUsername(username);
-        newUser.setPassword(password);
-        userService.add(newUser);
-    }
-
-    @PostMapping(value = "/auth")
-    public ResponseEntity auth(HttpServletRequest request) {
-        String login = request.getParameter("username");
-        String password = request.getParameter("password");
-        User user = userService.findBy(login, password);
-        String token = jwtProvider.generateToken(user.getUsername());
-        Map<Object, Object> response = new HashMap<>();
-        response.put("token", token);
-        return ResponseEntity.ok(response);
+        if (userService.isExistBy(username)) {
+            return ResponseEntity.badRequest().body("Error: Username is already taken!");
+        }
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+        userService.add(user);
+        return ResponseEntity.ok("User registered successfully!");
     }
 
 }
